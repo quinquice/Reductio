344810c32088aae1a26f2d1f8f0bd911ac813ba7
Charles Gunn <cjgunnmail@gmail.com>
2020-02-15 18:33:04 -0500

    tools version update

--- Package.swift
+++ Package.swift
@@ -1,13 +1,14 @@
-/**
- This file is part of the Reductio package.
- (c) Sergio Fernández <fdz.sergio@gmail.com>
-
- For the full copyright and license information, please view the LICENSE
- file that was distributed with this source code.
- */
-
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
-  name: "Reductio"
+  name: "Reductio",
+  products: [
+      .library(name: "Reductio", targets: ["Reductio"]),
+  ],
+  targets: [
+        .target(
+            name: "Reductio",
+            path: "Source"),
+    ]
 )
